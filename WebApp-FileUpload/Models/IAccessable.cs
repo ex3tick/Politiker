ce88d4f42d712cca6a8d7738e9a4c712dabf8621
ex3tick <148373740+ex3tick@users.noreply.github.com>
2024-03-14 10:36:12 +0100

    Interface

--- WebApp-FileUpload/Models/IAccessable.cs
+++ WebApp-FileUpload/Models/IAccessable.cs
@@ -2,5 +2,12 @@
 
 public interface IAccessable
 {
+    List<Politiker>getAllPolitikers();
+    Politiker getPolitikerById(int id);
+    int addPolitiker(Politiker politiker);
+    
+    bool updatePolitiker(Politiker politiker);
+    bool deletePolitiker(int id);
+    
     
 }
